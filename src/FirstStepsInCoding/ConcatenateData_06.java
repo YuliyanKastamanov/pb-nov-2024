6879795c3c115ce480e2b059ad85d8104b5d76e6
YuliyanKastamanov <92514049+YuliyanKastamanov@users.noreply.github.com>
2024-11-02 13:22:13 +0200

    Initial commit

---
+++ src/FirstStepsInCoding/ConcatenateData_06.java
@@ -0,0 +1,27 @@
+package FirstStepsInCoding;
+
+import java.util.Scanner;
+
+public class ConcatenateData_06 {
+
+    public static void main(String[] args) {
+
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        //Прочитаме входните данни от конзолата
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String town = scanner.nextLine();
+
+        System.out.printf("You are %s %s, a %d-years old person from %s.", firstName, lastName, age, town);
+
+        //System.out.println("You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + town + ".");
+    }
+
+
+}
